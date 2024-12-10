1e74a84351f136dd3cc5d747e79ed167195bba08
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-10 12:23:25 -0400

    [Netlify] Add github integration with onStar & onIssue functions

--- netlify/functions/github-discord/github-discord.ts
+++ netlify/functions/github-discord/github-discord.ts
@@ -19,8 +19,53 @@ const notify = async (message: string) => {
   return true;
 };
 
+const onStar = (payload: any): string => {
+  const { action, sender, repository, starred_at } = payload;
+
+  return `User ${sender.login} ${action} star on ${repository.full_name}`;
+};
+
+const onIssue = (payload: any): string => {
+  const { action, issue, repository, sender } = payload;
+
+  if (action === "opened") {
+    return `An issue was opened with this title ${issue.title}`;
+  }
+
+  if (action === "closed") {
+    return `An issue was closed by ${issue.user.login}`;
+  }
+
+  if (action === "reopened") {
+    return `An issue was reopened by ${issue.user.login}`;
+  }
+
+  return `Unhandled action for the issue event ${action}`;
+};
+
 export const handler: Handler = async (event, context) => {
-  await notify("Hola Mundo desde Netlify Dev");
+  const githubEvent = event.headers["x-github-event"] ?? "unknown";
+  const payload = JSON.parse(event.body ?? "{}");
+  let message: string;
+
+  console.log(payload);
+
+  switch (githubEvent) {
+    case "star":
+      message = onStar(payload);
+      break;
+
+    case "issues":
+      message = onIssue(payload);
+      break;
+
+    default:
+      message = `Unknown event ${githubEvent}`;
+
+      break;
+  }
+
+  await notify(message);
   return {
     body: JSON.stringify({ message: "done" }),
     statusCode: 200,
